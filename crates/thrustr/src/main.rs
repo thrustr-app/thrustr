6fcede05994849d45235079e8181556ae2a20ffe
Jorge Pardo <jparpar@proton.me>
2025-12-24 12:30:10 +0100

    feat: add gpui-tokio for compatibility

--- crates/thrustr/src/main.rs
+++ crates/thrustr/src/main.rs
@@ -1,14 +1,17 @@
-use std::sync::Arc;
-
 use crate::routes::Root;
 use gpui::{AppContext, Application, TitlebarOptions, WindowOptions};
 use plugin_manager::PluginManager;
 use sqlite_storage::SqliteStorage;
+use std::sync::Arc;
+use tokio::runtime;
 
 #[path = "routes/root.rs"]
 mod routes;
 
 fn main() {
+    let tokio_runtime = runtime::Builder::new_multi_thread().build().unwrap();
+
+    // TODO
     let exe_path = std::env::current_exe().unwrap();
     let exe_dir = exe_path.parent().unwrap();
     let db_path = exe_dir.join("thrustr.db");
@@ -21,8 +24,11 @@ fn main() {
 
     let plugins = plugin_manager.list_plugins();
     println!("Loaded Plugins: {:?}", plugins);
+    // TODO
 
-    Application::new().run(|app| {
+    Application::new().run(move |app| {
+        gpui_tokio::init_from_handle(app, tokio_runtime.handle().clone());
+
         app.open_window(
             WindowOptions {
                 titlebar: Some(TitlebarOptions {
