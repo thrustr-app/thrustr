f329bc40348a00d0fd3adb6d3dedf2142cd32923
Jorge Pardo <jparpar@proton.me>
2026-01-27 17:37:53 +0100

    feat(thrustr-plugin): methods to set and get booleans

--- crates/thrustr-plugin/src/kv_store.rs
+++ crates/thrustr-plugin/src/kv_store.rs
@@ -16,12 +16,30 @@ impl KvStore {
         Ok(get(key)?.map(|bytes| String::from_utf8(bytes).unwrap()))
     }
 
+    pub fn get_bool(key: &str) -> Result<Option<bool>, KvStoreError> {
+        Ok(get(key)?.map(|bytes| {
+            if bytes.len() != 1 {
+                panic!("Expected 1 byte for boolean value");
+            }
+            match bytes[0] {
+                0 => false,
+                1 => true,
+                _ => panic!("Invalid byte value for boolean: {}", bytes[0]),
+            }
+        }))
+    }
+
     pub fn set(key: &str, value: &[u8]) -> Result<(), KvStoreError> {
         Ok(set(key, value)?)
     }
 
     pub fn set_string(key: &str, value: &str) -> Result<(), KvStoreError> {
         Ok(set(key, value.as_bytes())?)
+    }
+
+    pub fn set_bool(key: &str, value: bool) -> Result<(), KvStoreError> {
+        let byte = if value { 1u8 } else { 0u8 };
+        Ok(set(key, &[byte])?)
     }
 
     pub fn delete(key: &str) -> Result<(), KvStoreError> {
