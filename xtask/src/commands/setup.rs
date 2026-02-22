003e314bd6064980cd5fc15a228e4b1a843c387a
Jorge Pardo <jparpar@proton.me>
2026-02-22 22:44:01 +0100

    chore: rename hooks to .hooks

--- xtask/src/commands/setup.rs
+++ xtask/src/commands/setup.rs
@@ -1,17 +1,10 @@
 use anyhow::Result;
-use std::{fs, process::Command};
+use std::fs;
 
 pub fn setup() -> Result<()> {
     println!("Setting up commit linting...");
 
-    let status = Command::new("cargo")
-        .args(["build", "--release", "-p", "commit_lint"])
-        .status()?;
-    if !status.success() {
-        anyhow::bail!("Failed to build commit_lint");
-    }
-
-    fs::copy("hooks/commit-msg", ".git/hooks/commit-msg")?;
+    fs::copy(".hooks/commit-msg", ".git/hooks/commit-msg")?;
     #[cfg(unix)]
     {
         use std::{fs::Permissions, os::unix::fs::PermissionsExt};
