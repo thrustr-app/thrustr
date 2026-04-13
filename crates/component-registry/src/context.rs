4c841d2ff2c90a25bbab87cdab7bb5f1085de0af
Jorge Pardo <jparpar@proton.me>
2026-04-13 13:16:51 +0200

    refactor: actually remove component-registry crate

--- crates/component-registry/src/context.rs
+++
@@ -1,8 +0,0 @@
-use domain::{component::ComponentStorage, game::GameRepository};
-use std::sync::Arc;
-
-#[derive(Clone)]
-pub struct RegistryContext {
-    pub component_storage: Arc<dyn ComponentStorage>,
-    pub game_storage: Arc<dyn GameRepository>,
-}
